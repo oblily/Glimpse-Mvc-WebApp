27a81f0403db5a9a4c24368e68247dc14d92d508
oblily <business.mail.kl@gmail.com>
2016-02-09 22:39:44 +0900

    Introduce Glimpse to MVC WebApp

---
+++ GlimpseMvcWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GlimpseMvcWebApp.MvcApplication" Language="C#" %>
---
+++ GlimpseMvcWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GlimpseMvcWebApp.Startup))]
+namespace GlimpseMvcWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
